ecc654df7e1bfa3a2b27e9cec9eef377add0eb84
Andrey Danilov <andrewdanilov@gmail.com>
2019-04-07 13:18:17 +0300

    misc

--- src/YandexMapAsset.php
+++ src/YandexMapAsset.php
@@ -5,7 +5,7 @@ namespace andrewdanilov\yandexmap;
 use yii\web\AssetBundle;
 
 class YandexMapAsset extends AssetBundle {
-	public $sourcePath = '@vendor/andrewdanilov/yii2-yandexmap/src/web';
+	public $sourcePath = '@andrewdanilov/yandexmap/web';
 	public $js = [
 		'//api-maps.yandex.ru/2.1/?lang=ru_RU',
 		'js/ym.js',
